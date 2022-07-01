a0cd8abfb527078b94faec80aae864aec7804905
hzhang20902 <85701939+hzhang20902@users.noreply.github.com>
2022-06-30 20:23:07 -0400

    abandon ship must rebuild from scratch

--- src/main/java/com/shelterapp/backend/ShelterApplication.java
+++ src/main/java/com/shelterapp/backend/ShelterApplication.java
@@ -2,10 +2,11 @@ package com.shelterapp.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-//@EnableJpaAuditing
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class ShelterApplication {
 
     public static void main(String[] args) {
